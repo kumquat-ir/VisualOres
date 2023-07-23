35b1479ccb3d6be08656d2cd61a05bbcb79ca850
kumquat-ir <66188216+kumquat-ir@users.noreply.github.com>
2023-07-22 20:58:54 -0400

    :gun: tabs

--- src/main/java/hellfall/visualores/mixins/journeymap/FullscreenMixin.java
+++ src/main/java/hellfall/visualores/mixins/journeymap/FullscreenMixin.java
@@ -34,112 +34,112 @@ import java.util.*;
 @Mixin(Fullscreen.class)
 public abstract class FullscreenMixin extends JmUI implements ITabCompleter {
 
-	@Unique private JourneymapRenderer renderer;
+    @Unique private JourneymapRenderer renderer;
 
-	@Shadow ThemeToolbar mapTypeToolbar;
+    @Shadow ThemeToolbar mapTypeToolbar;
 
-	@Shadow @Final static MapState state;
+    @Shadow @Final static MapState state;
 
-	@Shadow @Final static GridRenderer gridRenderer;
+    @Shadow @Final static GridRenderer gridRenderer;
 
-	@Shadow FullMapProperties fullMapProperties;
+    @Shadow FullMapProperties fullMapProperties;
 
-	@Unique private Map<String, ThemeToggle> buttons;
+    @Unique private Map<String, ThemeToggle> buttons;
 
-	@Unique private ThemeToolbar overlayToolbar;
+    @Unique private ThemeToolbar overlayToolbar;
 
-	public FullscreenMixin(String title) {
-		super(title);
-	}
+    public FullscreenMixin(String title) {
+        super(title);
+    }
 
-	@Inject(method = "initButtons",
-			at = @At(value = "FIELD",
-					target = "Ljourneymap/client/ui/fullscreen/Fullscreen;mapTypeToolbar:Ljourneymap/client/ui/theme/ThemeToolbar;",
-					opcode = Opcodes.PUTFIELD,
-					shift = At.Shift.AFTER
-			), remap = false
-	)
-	private void visualores$injectInitButtons(CallbackInfo ci) {
-		final Theme theme = ThemeLoader.getCurrentTheme();
-		buttons = new LinkedHashMap<>();
+    @Inject(method = "initButtons",
+            at = @At(value = "FIELD",
+                    target = "Ljourneymap/client/ui/fullscreen/Fullscreen;mapTypeToolbar:Ljourneymap/client/ui/theme/ThemeToolbar;",
+                    opcode = Opcodes.PUTFIELD,
+                    shift = At.Shift.AFTER
+            ), remap = false
+    )
+    private void visualores$injectInitButtons(CallbackInfo ci) {
+        final Theme theme = ThemeLoader.getCurrentTheme();
+        buttons = new LinkedHashMap<>();
 
-		for (ButtonState.Button button : ButtonState.getAllButtons()) {
-			ThemeToggle mapButton = new ThemeToggle(theme, "visualores.button." + button.name, button.name);
-			mapButton.setToggled(ButtonState.isEnabled(button), false);
-			mapButton.setEnabled(true);
-			mapButton.addToggleListener((onOffButton, b) -> {
-				ButtonState.toggleButton(button);
+        for (ButtonState.Button button : ButtonState.getAllButtons()) {
+            ThemeToggle mapButton = new ThemeToggle(theme, "visualores.button." + button.name, button.name);
+            mapButton.setToggled(ButtonState.isEnabled(button), false);
+            mapButton.setEnabled(true);
+            mapButton.addToggleListener((onOffButton, b) -> {
+                ButtonState.toggleButton(button);
 
-				return true;
-			});
+                return true;
+            });
 
-			buttons.put(button.name, mapButton);
-		}
+            buttons.put(button.name, mapButton);
+        }
 
-		List<ThemeToggle> allButtons = new ArrayList<>(buttons.values());
-		Collections.reverse(allButtons);
+        List<ThemeToggle> allButtons = new ArrayList<>(buttons.values());
+        Collections.reverse(allButtons);
 
-		if (VOConfig.client.jmap.rightToolbar) {
-			overlayToolbar = new ThemeToolbar(theme, allButtons.toArray(new Button[0]));
-			overlayToolbar.setLayout(ButtonList.Layout.Vertical, ButtonList.Direction.RightToLeft);
-			overlayToolbar.addAllButtons((Fullscreen) (Object) this);
-		}
-		else {
-			// jank to not have to add an accessor/at
-			this.mapTypeToolbar.reverse();
-			this.mapTypeToolbar.reverse().addAll(0, allButtons);
-		}
+        if (VOConfig.client.jmap.rightToolbar) {
+            overlayToolbar = new ThemeToolbar(theme, allButtons.toArray(new Button[0]));
+            overlayToolbar.setLayout(ButtonList.Layout.Vertical, ButtonList.Direction.RightToLeft);
+            overlayToolbar.addAllButtons((Fullscreen) (Object) this);
+        }
+        else {
+            // jank to not have to add an accessor/at
+            this.mapTypeToolbar.reverse();
+            this.mapTypeToolbar.reverse().addAll(0, allButtons);
+        }
 
 
-		renderer = new JourneymapRenderer((Fullscreen) (Object) this);
-	}
+        renderer = new JourneymapRenderer((Fullscreen) (Object) this);
+    }
 
-	@Inject(method = "layoutButtons", at = @At("TAIL"), remap = false)
-	private void visualores$injectLayoutButtons(CallbackInfo ci) {
-		for (String buttonName : buttons.keySet()) {
-			buttons.get(buttonName).setToggled(ButtonState.isEnabled(buttonName), false);
-		}
+    @Inject(method = "layoutButtons", at = @At("TAIL"), remap = false)
+    private void visualores$injectLayoutButtons(CallbackInfo ci) {
+        for (String buttonName : buttons.keySet()) {
+            buttons.get(buttonName).setToggled(ButtonState.isEnabled(buttonName), false);
+        }
 
-		if (VOConfig.client.jmap.rightToolbar) {
-			overlayToolbar.layoutCenteredVertical(width - overlayToolbar.getHMargin(), height / 2, false, mapTypeToolbar.getToolbarSpec().padding);
-		}
-	}
+        if (VOConfig.client.jmap.rightToolbar) {
+            overlayToolbar.layoutCenteredVertical(width - overlayToolbar.getHMargin(), height / 2, false, mapTypeToolbar.getToolbarSpec().padding);
+        }
+    }
 
-	@Redirect(method = "drawMap",
-			at = @At(value = "INVOKE", target = "Ljourneymap/client/render/map/GridRenderer;draw(Ljava/util/List;DDDD)V", ordinal = 1),
-			remap = false
-	)
-	private void visualores$injectDrawMap(GridRenderer instance, List<? extends DrawStep> drawStepList, double xOffset, double yOffset, double fontScale, double rotation) {
-		instance.draw(drawStepList, xOffset, yOffset, fontScale, rotation);
+    @Redirect(method = "drawMap",
+            at = @At(value = "INVOKE", target = "Ljourneymap/client/render/map/GridRenderer;draw(Ljava/util/List;DDDD)V", ordinal = 1),
+            remap = false
+    )
+    private void visualores$injectDrawMap(GridRenderer instance, List<? extends DrawStep> drawStepList, double xOffset, double yOffset, double fontScale, double rotation) {
+        instance.draw(drawStepList, xOffset, yOffset, fontScale, rotation);
 
-		double scale = Math.pow(2, fullMapProperties.zoomLevel.get());
-		double rw = mc.displayWidth / scale;
-		double rh = mc.displayHeight / scale;
-		renderer.updateVisibleArea(state.getDimension(), (int) (gridRenderer.getCenterBlockX() - xOffset - rw / 2), (int) (gridRenderer.getCenterBlockZ() - yOffset - rh / 2), (int) rw, (int) rh);
+        double scale = Math.pow(2, fullMapProperties.zoomLevel.get());
+        double rw = mc.displayWidth / scale;
+        double rh = mc.displayHeight / scale;
+        renderer.updateVisibleArea(state.getDimension(), (int) (gridRenderer.getCenterBlockX() - xOffset - rw / 2), (int) (gridRenderer.getCenterBlockZ() - yOffset - rh / 2), (int) rw, (int) rh);
 
-		GlStateManager.pushMatrix();
-		GlStateManager.translate(xOffset, yOffset, 0);
-		GlStateManager.scale(scale, scale, 1);
-		GlStateManager.translate(rw / 2, rh / 2, 0);
+        GlStateManager.pushMatrix();
+        GlStateManager.translate(xOffset, yOffset, 0);
+        GlStateManager.scale(scale, scale, 1);
+        GlStateManager.translate(rw / 2, rh / 2, 0);
 
-		renderer.render(gridRenderer.getCenterBlockX(), gridRenderer.getCenterBlockZ(), scale);
+        renderer.render(gridRenderer.getCenterBlockX(), gridRenderer.getCenterBlockZ(), scale);
 
-		GlStateManager.popMatrix();
-	}
+        GlStateManager.popMatrix();
+    }
 
-	@Inject(method = "drawScreen", at = @At(value = "INVOKE", target = "Ljava/util/List;isEmpty()Z", shift = At.Shift.BY, by = -2), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
-	private void visualores$injectTooltip(int scaledMouseX, int scaledMouseY, float partialTicks, CallbackInfo ci, List<String> tooltip) {
-		double scale = Math.pow(2, fullMapProperties.zoomLevel.get());
-		renderer.updateHovered(scaledMouseX, scaledMouseY, gridRenderer.getCenterBlockX(), gridRenderer.getCenterBlockZ(), scale);
-		if (tooltip == null || tooltip.isEmpty()) {
-			renderer.renderTooltip(scaledMouseX, scaledMouseY);
-		}
-	}
+    @Inject(method = "drawScreen", at = @At(value = "INVOKE", target = "Ljava/util/List;isEmpty()Z", shift = At.Shift.BY, by = -2), locals = LocalCapture.CAPTURE_FAILEXCEPTION)
+    private void visualores$injectTooltip(int scaledMouseX, int scaledMouseY, float partialTicks, CallbackInfo ci, List<String> tooltip) {
+        double scale = Math.pow(2, fullMapProperties.zoomLevel.get());
+        renderer.updateHovered(scaledMouseX, scaledMouseY, gridRenderer.getCenterBlockX(), gridRenderer.getCenterBlockZ(), scale);
+        if (tooltip == null || tooltip.isEmpty()) {
+            renderer.renderTooltip(scaledMouseX, scaledMouseY);
+        }
+    }
 
-	@Inject(method = "keyTyped", at = @At("TAIL"))
-	private void visualores$injectKeyPress(char c, int key, CallbackInfo ci) {
-		if (KeyBindings.action.getKeyCode() == key) {
-			renderer.onActionKey();
-		}
-	}
+    @Inject(method = "keyTyped", at = @At("TAIL"))
+    private void visualores$injectKeyPress(char c, int key, CallbackInfo ci) {
+        if (KeyBindings.action.getKeyCode() == key) {
+            renderer.onActionKey();
+        }
+    }
 }
